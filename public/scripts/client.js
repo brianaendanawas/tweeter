dabb3305bae345155ec35481ec3873fa1a8521b7
Briana Endanawas <brianamay48@gmail.com>
2021-12-31 22:47:08 -0500

    add section to  html

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -40,24 +40,28 @@ $(document).ready(function() {
   
   const createTweetElement = function(tweet) {
     const $tweet = 
-    $(`<article class="tweet-top">
-      <div class="picture-and-name">
-        <img src="${tweet.user.avatars}">
-        <p class="name">${tweet.user.name}</p>
-      </div>
-      <p class="handle">${tweet.user.handle}</p>
-    </article>
-    <article class="tweet-middle">
-      <p>${tweet.content.text}</p>
-    </article>
-    <footer>
-      <p>${tweet.created_at}</p>
-      <div class="buttons">
-        <i class="fas fa-flag"></i>
-        <i class="fas fa-retweet"></i>
-        <i class="fas fa-heart"></i>
-      </div>
-    </footer>`);
+    $(`
+    <section class="tweets">
+      <article class="tweet-top">
+        <div class="picture-and-name">
+          <img src="${tweet.user.avatars}">
+          <p class="name">${tweet.user.name}</p>
+        </div>
+        <p class="handle">${tweet.user.handle}</p>
+      </article>
+      <article class="tweet-middle">
+        <p>${tweet.content.text}</p>
+      </article>
+      <footer>
+        <p>${tweet.created_at}</p>
+        <div class="buttons">
+          <i class="fas fa-flag"></i>
+          <i class="fas fa-retweet"></i>
+          <i class="fas fa-heart"></i>
+        </div>
+      </footer>
+    </section>
+    `);
     return $tweet;
   }
   
